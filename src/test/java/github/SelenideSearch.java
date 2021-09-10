5bab9829c236027df9d307be59394af0c5ba1598
Valentina <15valentinka@gmail.com>
2021-09-10 08:48:13 +0500

    first

--- src/test/java/github/SelenideSearch.java
+++ src/test/java/github/SelenideSearch.java
@@ -9,19 +9,16 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSearch {
     @Test
-    void shouldFindSelenideRepositorePage(){ //слово Test необязательно при испо-ии gradle, в maven обязательно
+    void shouldFindSelenideRepositorePage() { //слово Test необязательно при испо-ии gradle, в maven обязательно
         //открыть страницу гитхаб
-       // open("https://github.com/");
-        ;
-        System.out.println("ИИИР");
-        //ввести в поле поиска  //нажать enter
-      //  $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
         // $$(".repo-list li").first().$("a").click(); //ищем коллекцию элементов, берем из нее первый внутри него ищем еще
-
-      //  var ListOfRepos =  $$(".repo-list li");
-     //   ListOfRepos.first().$("a").click();
+        var ListOfRepos = $$(".repo-list li");
         //Нажимаем на линк от первого результата поиска
-       // $("h1").should(text("selenide / selenide"));
+        ListOfRepos.first().$("a").click();
         //check: в заголовке встречается selenide/selenide
+        $("h1").should(text("selenide / selenide"));
+
     }
 }
